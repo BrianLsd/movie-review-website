e284380851ea848dba1f8afe6a467df9f57aff6f
Zhe Li <zhe.li99@ed.cna.nl.ca>
2023-02-08 18:33:41 -0330

    Used React Router to create a two-paged site with a nav bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
-function App() {
+export function Home(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/leaveReview">Leave Review</Link></li>
+        </ul>
+      </nav>
+      <h1>Movie Review</h1>
     </div>
   );
+}
+
+export function MovieForm(){
+  return (
+    <div>
+      <nav>
+        <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/leaveReview">Leave Review</Link></li>
+        </ul>
+      </nav>
+      <h1>Movie Form</h1>
+    </div>
+  );
+}
+
+function App() {
+  return <Home/>;
 }
 
 export default App;
