42134cfb38bace5c313e3fe6b8388f181db8c537
Zhe Li <zhe.li99@ed.cna.nl.ca>
2023-02-24 20:25:49 -0330

    Set up MongoDB, and made data loaded from db.

--- movie-review-backend/src/server.js
+++ movie-review-backend/src/server.js
@@ -1,10 +1,29 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// load movies
+app.get('/api/movies', async (req, res) => {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+
+    const db = client.db('movie-review-db');
+    const data = await db.collection('movies').find().toArray();
+    console.log(data);
+    res.json(data);
 });
+
+
+// add a new movie
+app.post('/api/leave-review', async (req, res) => {
+    const client = new MongoClient('mongodb://127.0.0.1:27017/');
+    await client.connect();
+
+    const db = client.db('movie-review-db');
+    //const insertOperation = 
+    res.redirect('/');
+})
 
 app.listen(port, () => console.log(`Server is running on localhost:${port}`));
